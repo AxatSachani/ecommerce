6f6589e36332d5ccc3201681cf557311ca8ef5ee
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-06-18 12:44:56 +0530

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose')
 const chalk = require('chalk')
 
-
-// const database = 'ecommerce'
-// const url = `mongodb+srv://beact:Admin%40123@ecommerce.q6bo7w0.mongodb.net/${database}?retryWrites=true&w=majority`
-//mongodb+srv://beact:Admin%40123@ecommerce.q6bo7w0.mongodb.net/ecommerce
-
-
 const database = 'ecommerce-demo'
-const url = `mongodb+srv://beact:Admin%40123@ecommerce-demo.xaafsoc.mongodb.net/${database}?retryWrites=true&w=majority`
-//mongodb+srv://beact:Admin%40123@ecommerce-demo.xaafsoc.mongodb.net/ecommerce-demo
+const url = `mongodb://localhost:27021/${database}?retryWrites=true&w=majority`
 
 var connectd = false
 mongoose.connect(url, {
